ca50c8e642f3e0618bb3701f6a34eb371b199849
JoanGaliana <jgalianamagan@gmail.com>
2022-05-03 00:46:42 +0200

    Removed unused import

--- back/src/test/java/com/galiana/tfg/service/UserServiceTests.java
+++ back/src/test/java/com/galiana/tfg/service/UserServiceTests.java
@@ -1,6 +1,5 @@
 package com.galiana.tfg.service;
 
-import com.galiana.tfg.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,9 +14,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class UserServiceTests {
     @Autowired
     private UserService userService;
-
-    @Autowired
-    private UserRepository userRepository;
 
     @Test
     @Transactional
