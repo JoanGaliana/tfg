dd67abcbe224258a1a7b7b55a9c73d317e8ec6e5
JoanGaliana <jgalianamagan@gmail.com>
2022-05-02 19:00:31 +0200

    Invalid login password request mock test

---
+++ back/src/test/java/com/galiana/tfg/api/UsersTest.java
@@ -0,0 +1,37 @@
+package com.galiana.tfg.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UsersTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void invalidLoginPasswordReturnsUnauthorized() throws Exception {
+        var body = """
+                                {
+                                  "email": "alicia@test.com",
+                                  "password": "123456"
+                                }
+                """;
+
+        this.mockMvc.perform(
+                        post("/login")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(body)
+                )
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.error").value("INVALID_CREDENTIALS"));
+    }
+}
