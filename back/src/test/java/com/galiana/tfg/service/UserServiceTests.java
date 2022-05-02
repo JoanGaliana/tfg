82ae56f7cd70f5f958793c6018b464383d11e0bb
JoanGaliana <jgalianamagan@gmail.com>
2022-05-02 18:15:18 +0200

    User service already exists test

--- back/src/test/java/com/galiana/tfg/service/UserServiceTests.java
+++ back/src/test/java/com/galiana/tfg/service/UserServiceTests.java
@@ -5,7 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
+import javax.xml.bind.ValidationException;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 public class UserServiceTests {
@@ -16,6 +20,7 @@ public class UserServiceTests {
     private UserRepository userRepository;
 
     @Test
+    @Transactional
     void create() throws Exception {
         var email = "a@test.net";
         var password = "password";
@@ -25,5 +30,18 @@ public class UserServiceTests {
         assertThat(newUser).isNotNull();
         assertThat(newUser.getEmail()).isEqualTo(email);
         assertThat(newUser.getPassword()).isNotEqualTo(email);
+    }
+
+    @Test
+    @Transactional
+    void createAlreadyExistingEmail() {
+        var existingEmail = "alicia@test.com";
+        var password = "password";
+
+        assertThatThrownBy(() -> {
+            userService.create(existingEmail, password);
+        })
+                .isInstanceOf(ValidationException.class)
+                .hasMessageContaining("Username already exists");
     }
 }
